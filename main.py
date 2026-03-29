e165015ddc589caa7188a097472a4badea7b729d
AbnerYousuf <yousufabner@gmail.com>
2026-03-28 23:35:41 -0600

    tested adding multiple pages

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template, request
 import os
 
-app = Flask(__name__)
+app = Flask("WeatherAPI")
 
 @app.route('/')
 def home():
     return render_template('frontend.html')
 
+@app.route('/about/')
+def about():
+    return render_template('about.html')
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
