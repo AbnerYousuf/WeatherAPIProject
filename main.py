d12187f42c0d0ea617dff1a54c0c1705ed127853
AbnerYousuf <yousufabner@gmail.com>
2026-03-29 17:37:06 -0600

    built a dummy version of the API with hardcoded temperature and skeleton outine of how we're gonna read the data

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 from flask import Flask, render_template, request
+import pandas
 import os
 
-app = Flask("WeatherAPI")
+app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('frontend.html')
 
-@app.route('/about/')
+@app.route('/documentation/')
 def about():
     return render_template('about.html')
+
+@app.route('/weather/<station>/<date>')
+def weather(station, date):
+    #df = pandas.read_csv()
+    #temperature = df[]['temperature'].values
+    temperature = 67
+    return {
+        'station': station,
+        'date': date,
+        'temperature': temperature
+    }
 
 if __name__ == '__main__':
     app.run(debug=True)
